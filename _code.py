08c3ca64b29ce760fa961cad06a765a734326212
CuongDaNezu <dvcuong.work@gmail.com>
2024-06-25 00:05:53 +0700

    add knn_labels and predict function

--- _code.py
+++ _code.py
@@ -1,14 +1,8 @@
 from transformers import AutoModel, AutoTokenizer
 import torch
-from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics.pairwise import euclidean_distances, cosine_similarity
 import numpy as np
 import re
-
-def knn_labels(points, centers):
-    distance = euclidean_distances(points, centers)
-    distance = distance**2
-    labels = np.argmin(distance, axis=1)
-    return labels
 
 def load_model_tokenizer():
     model = AutoModel.from_pretrained('./model')
@@ -68,4 +62,61 @@ def vectorize_context(context, QA, model, tokenizer):
     for ans in QA['answer_options']:
         new_vec_ans_opt = vectorize_sentence(ans, model, tokenizer)
         vec_answer_options.append(new_vec_ans_opt)
-    return vec_context, vec_question, vec_answer_options
+    return sens, vec_context, vec_question, vec_answer_options
+
+def select_sens(sens, vec_context, vec_question):
+    #threads hold: cosine:
+    e = 0.6
+    
+    i = 0
+    while(True):
+        if(len(sens) == 5):
+            break
+        if(i == len(sens)):
+            break
+        cosine_sim = cosine_similarity(vec_question, vec_context[i])
+        if cosine_sim <= e:
+            sens.pop(i)
+            vec_context.pop(i)
+        else:
+            i+=1
+    return sens, vec_context, vec_question
+
+def knn_labels(points, centers):
+    similarity = cosine_similarity(points, centers)
+    distance = 1 - similarity
+    # Find the index of the center with the highest similarity (smallest distance)
+    labels = np.argmin(distance, axis=1)
+
+
+    # Compute the mean distance (cost) for each center
+    min_distances = distance[np.arange(distance.shape[0]), labels]
+    costs = []
+    for center_idx in range(centers.shape[0]):
+        center_distances = min_distances[labels == center_idx]
+        if len(center_distances) > 0:
+            mean_distance = np.mean(center_distances)
+        else:
+            mean_distance = 0  # No points assigned to this center
+        costs.append(mean_distance)
+    
+    return labels, costs
+
+def predict(labels, costs, sens):
+    sum_cost = costs.sum()
+    percentage_answers = [
+        costs[0]/sum_cost,
+        costs[1]/sum_cost,
+        costs[2]/sum_cost,
+        costs[3]/sum_cost,
+    ]
+    pred_ans_ratio = max(percentage_answers)
+    pred_ans = percentage_answers.index(pred_ans_ratio)
+    explain = ""
+    i = 0
+    while(True):
+        if(i==len(sens)):
+            break
+        if(labels[i]==pred_ans):
+            explain += (sens[i] +". ")
+    return pred_ans, pred_ans_ratio, explain
